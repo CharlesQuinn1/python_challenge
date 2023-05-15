e4358a59477d5ba56f48130635bdad403023125f
CharlesQuinn1 <128498023+CharlesQuinn1@users.noreply.github.com>
2023-05-15 18:50:12 -0500

    Update file

---
+++ PyPoll/new 1.py
@@ -0,0 +1,10 @@
+another_actor = {
+    "name": "Sylvester Stallone",
+    "age": 62,
+    "married": True,
+    "best movies": [
+        "Rocky",
+        "Rocky 2",
+        "Rocky 3"]}
+#print(f'{another_actor["name"]} was in {another_actor["best movies"][0]}')
+print(another_actor)
